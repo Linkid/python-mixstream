06f9523e071f16c2c2f663d10ef23de1b3223436
François Magimel <magimel.francois@gmail.com>
2023-01-01 17:21:08 +0100

    ci(gha): install vcpkg packages before building windows wheels

--- .github/workflows/before_build.sh
+++ .github/workflows/before_build.sh
@@ -35,6 +35,23 @@ case ${operating_system} in
             sdl2_mixer \
             sound-touch
     ;;
+    "windows"*)
+        echo "[*] windows"
+        echo "- cibw_build: ${CIBW_BUILD}"
+        echo "- cibw_arch: ${CIBW_ARCH}"
+
+        echo "VCPKG_ROOT: .${VCPKG_ROOT}."
+        ${VCPKG_ROOT}/vcpkg install \
+            glib \
+            libvorbis \
+            portmidi \
+            sdl2-mixer \
+            soundtouch --clean-after-build
+
+        echo "[*] integrate"
+        ${VCPKG_ROOT}/vcpkg integrate install
+
+    ;;
     *)
         echo "[*] not supported OS: " ${operating_system}
     ;;
